be41d09d4c252c2d85551c59a87c3c89b2750380
Mehul Mittal <mehul.usc@gmail.com>
2014-03-31 15:55:40 -0700

    update sms.js

--- www/sms.js
+++ www/sms.js
@@ -1,12 +1,12 @@
-var smsPlugin = {
+var sms = {
     createEvent: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'SmsPlugin', // mapped to our native Java class called "Calendar"
-            'openSmsView', // with this action name
+            'openSmsView' // with this action name
         ); 
      }
-}
+};
 
 module.exports = sms;
