3a22d224e2c696c765407f7571735bf80f3efe5a
Mehul Mittal <mehul.usc@gmail.com>
2014-03-31 16:25:19 -0700

    Update sms.js

--- www/sms.js
+++ www/sms.js
@@ -5,7 +5,6 @@ var sms = {
             errorCallback, // error callback function
             'SmsPlugin', // mapped to our native Java class called "Calendar"
             'openSmsView', // with this action name
-            , // with this action name
             [{                  // and this array of custom arguments to create our entry
             }]
         ); 
